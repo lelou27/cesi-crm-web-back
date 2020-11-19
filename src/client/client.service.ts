4995f592c2ab4143f2d8723170e84c0261917381
Benjamin <benjamin.gille@gmail.com>
2020-11-19 10:27:19 +0100

    Ajout méthode update + delete Client

--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Client, ClientDocument } from '../Schemas/client.schema';
 import { Model } from 'mongoose';
+import { UpdateClientDto } from '../Dto/UpdateClientDto';
 
 @Injectable()
 export class ClientService {
@@ -33,5 +34,40 @@ export class ClientService {
 
   async getAllClient(): Promise<Client[]> {
     return await this.ClientModel.find().populate('client').exec();
+  }
+
+  async deleteClient(clientId: String): Promise<any> {
+    try {
+      return await this.ClientModel.deleteOne({ _id: clientId });
+    } catch (e) {
+      throw new HttpException(
+        'Impossible de supprimer de le client',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async updateClient(clientId: string, clientData: UpdateClientDto): Promise<Client> {
+    try {
+      const client = await this.ClientModel.findByIdAndUpdate(
+        clientId,
+        {
+          first_name: clientData.first_name,
+          mail: clientData.mail,
+          phone: clientData.phone,
+          address: clientData.address,
+          postal_code: clientData.postal_code,
+          city: clientData.city,
+          country: clientData.country,
+        },
+        { new: true },
+      );
+      return client;
+    } catch (e) {
+      throw new HttpException(
+        'Impossible de mettre à jour l\'utilisateur',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
