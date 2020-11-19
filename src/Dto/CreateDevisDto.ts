9218aa149a929ddd7b8aabbbbcf6755f0ecc624b
lelou27 <lelou.pro@gmail.com>
2020-11-19 11:43:19 +0100

    Feat : Ajout des champs dans le devis

--- src/Dto/CreateDevisDto.ts
+++ src/Dto/CreateDevisDto.ts
@@ -1,8 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Client } from '../Schemas/client.schema';
+import { Moment } from 'moment';
 
 export class CreateDevisDto {
   @ApiProperty() readonly nomProjet: string;
   @ApiProperty() readonly client: Client;
-  @ApiProperty() readonly dateDevis: Date;
+  @ApiProperty() readonly dateDevis: Moment;
 }
